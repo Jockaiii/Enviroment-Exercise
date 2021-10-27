c41523f271092c710476f4fbcc9e2490f4f744de
Joakim Andersson <joakimandersson10@gmail.com>
2021-10-27 10:45:50 +0200

    Commit for testing enviroment 1

--- Enviroment Exercise/Program.cs
+++ Enviroment Exercise/Program.cs
@@ -4,7 +4,7 @@ namespace Enviroment_Exercise
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
         }
